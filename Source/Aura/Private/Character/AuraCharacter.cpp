d5b45e9097b72ad0ddcfceab4495234821bd3670
Eyishi <15922751294@163.com>
2025-03-12 23:26:40 +0800

    Add Enemy And Aura Character Class

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Eyishi
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Eyishi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
