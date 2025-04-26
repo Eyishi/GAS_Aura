25ca4976d0e685603d134423161e2ff216fdacc2
Eyishi <15922751294@163.com>
2025-04-26 16:14:49 +0800

    通过gameplayCue 播放攻击特效和声音

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -27,15 +27,15 @@ public:
 	/** EnemyInterface  */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
+	virtual AActor* GetCombatTarget_Implementation() const override;
 	/**End EnemyInterface  */
 
 	/** CombatInterface  */
 	virtual int32 GetPlayerLevel() override;
 
 	virtual void Die() override;
-
-	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
-	virtual AActor* GetCombatTarget_Implementation() const override;
+	
 	/**End CombatInterface  */
 
 	UPROPERTY(BlueprintAssignable)
